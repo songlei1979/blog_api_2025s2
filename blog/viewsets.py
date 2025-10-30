4a6726f4d62044b86be4ad1279c5d975400d3713
Lei <gabriel_sl19798@hotmail.com>
2025-10-31 11:41:26 +1300

    created permission class

--- blog/viewsets.py
+++ blog/viewsets.py
@@ -1,16 +1,20 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
 from blog.models import Category, Post, Comment, Profile
+from blog.permissions import isAuthorOrReadOnly
 from blog.serializers import CategorySerializer, PostSerializer, CommentSerializer, ProfileSerializer
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAdminUser]
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [isAuthorOrReadOnly]
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
