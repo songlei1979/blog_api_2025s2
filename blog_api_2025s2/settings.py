bde8f3f7a68f9715c382df4678d297bab24bfc9a
Lei <gabriel_sl19798@hotmail.com>
2025-10-31 11:31:50 +1300

    add default permission class

--- blog_api_2025s2/settings.py
+++ blog_api_2025s2/settings.py
@@ -134,5 +134,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permissions.AllowAny',
     ]
 }
